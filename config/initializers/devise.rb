d8d7c75b2ba4bc9ca9f647ae64eccb2b5b9f8ccd
Tim Gourley <tgourley@gmail.com>
2017-06-07 22:40:57 -0500

    fixing smtp from address

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -17,7 +17,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'tim@night.coffee'
+  config.mailer_sender = ENV['SMTP_FROM_ADDRESS'] || 'notifications@localhost'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
